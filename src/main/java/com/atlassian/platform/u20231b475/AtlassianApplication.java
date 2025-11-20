4f041979cb4f8c2bed74f753271d18bf7dead3f0
Trevor <valentinosolischang@gmail.com>
2025-11-20 16:57:15 -0500

    Enable JPA auditing in AtlassianApplication

--- src/main/java/com/atlassian/platform/u20231b475/AtlassianApplication.java
+++ src/main/java/com/atlassian/platform/u20231b475/AtlassianApplication.java
@@ -2,8 +2,10 @@ package com.atlassian.platform.u20231b475;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AtlassianApplication {
 
 	public static void main(String[] args) {
