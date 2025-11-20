c1962b1c6d74857c89aaf421775daca8fbdf8fb7
Trevor <valentinosolischang@gmail.com>
2025-11-20 15:22:34 -0500

    Add AccessRequestResource record for access request representation

--- src/main/java/com/atlassian/platform/u20231b475/requests/interfaces/rest/resources/AccessRequestResource.java
+++ src/main/java/com/atlassian/platform/u20231b475/requests/interfaces/rest/resources/AccessRequestResource.java
@@ -1,5 +1,17 @@
 package com.atlassian.platform.u20231b475.requests.interfaces.rest.resources;
 
-public record AccessRequestResource() {
+import java.time.LocalDateTime;
 
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.AccessLevel;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.DateRange;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.EmployeeId;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.Justification;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.RequestStatus;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.SystemName;
+
+/**
+ * Resource for an access request.
+ */
+public record AccessRequestResource(Long id, EmployeeId employeeId, SystemName requestedSystem, AccessLevel accessLevel,
+        Justification justification, DateRange accessPeriod, RequestStatus status, LocalDateTime requestedAt) {
 }
