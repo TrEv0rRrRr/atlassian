63832547f59810db8abbc3142a3a269752f77d10
Trevor <valentinosolischang@gmail.com>
2025-11-20 13:26:14 -0500

    Add initial test class for application context loading

---
+++ src/test/java/com/atlassian/platform/u20231b475/AtlassianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlassian.platform.u20231b475;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlassianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
