e91bdb5823402bb0af7844179ef9b5c36700e610
Trevor <valentinosolischang@gmail.com>
2025-11-20 15:22:51 -0500

    Refactor CreateAccessRequestCommandFromResourceAssembler for improved structure and clarity

--- src/main/java/com/atlassian/platform/u20231b475/requests/interfaces/rest/transform/CreateAccessRequestCommandFromResourceAssembler.java
+++ src/main/java/com/atlassian/platform/u20231b475/requests/interfaces/rest/transform/CreateAccessRequestCommandFromResourceAssembler.java
@@ -1,5 +1,28 @@
 package com.atlassian.platform.u20231b475.requests.interfaces.rest.transform;
 
-public class CreateAccessRequestCommandFromResourceAssembler {
+import com.atlassian.platform.u20231b475.requests.domain.model.commands.CreateAccessRequestCommand;
+import com.atlassian.platform.u20231b475.requests.interfaces.rest.resources.CreateAccessRequestResource;
 
+/**
+ * Assembler to convert a CreateAccessRequestResource to a
+ * CreateAccessRequestCommand.
+ */
+public class CreateAccessRequestCommandFromResourceAssembler {
+  /**
+   * Converts a CreateAccessRequestResource to a CreateAccessRequestCommand
+   * 
+   * @param employeeId The employee Id
+   * @param resource   The {@link CreateAccessRequestResource} resource to convert
+   * @return The {@link CreateAccessRequestCommand} command
+   */
+  public static CreateAccessRequestCommand toCommandFromResource(Long employeeId,
+      CreateAccessRequestResource resource) {
+    return new CreateAccessRequestCommand(
+        employeeId,
+        resource.requestName(),
+        resource.accessLevel(),
+        resource.justification(),
+        resource.startDate(),
+        resource.endDate());
+  }
 }
