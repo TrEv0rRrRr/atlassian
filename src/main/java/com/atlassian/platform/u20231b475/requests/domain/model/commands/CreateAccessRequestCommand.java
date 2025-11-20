f2e9ec95001756dbce6dda9ee350b542a7df85f4
Trevor <valentinosolischang@gmail.com>
2025-11-20 13:56:22 -0500

    Add CreateAccessRequestCommand for handling access request creation

---
+++ src/main/java/com/atlassian/platform/u20231b475/requests/domain/model/commands/CreateAccessRequestCommand.java
@@ -0,0 +1,17 @@
+package com.atlassian.platform.u20231b475.requests.domain.model.commands;
+
+import java.time.LocalDateTime;
+
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.AccessLevel;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.Justification;
+import com.atlassian.platform.u20231b475.requests.domain.model.valueobjects.SystemName;
+
+/**
+ * Command for Access Request creation
+ *
+ * @author Valentino Solis
+ */
+public record CreateAccessRequestCommand(Long employeeId, SystemName requestedSystem, AccessLevel accessLevel,
+    Justification justification, LocalDateTime startDate,
+    LocalDateTime endDate) {
+}
