4d687e1a2dee400e3b3f50e27ba82913abaf6938
Trevor <valentinosolischang@gmail.com>
2025-11-20 15:22:45 -0500

    Refactor AccessRequestResourceFromEntityAssembler for improved clarity and structure

--- src/main/java/com/atlassian/platform/u20231b475/requests/interfaces/rest/transform/AccessRequestResourceFromEntityAssembler.java
+++ src/main/java/com/atlassian/platform/u20231b475/requests/interfaces/rest/transform/AccessRequestResourceFromEntityAssembler.java
@@ -1,5 +1,27 @@
 package com.atlassian.platform.u20231b475.requests.interfaces.rest.transform;
 
-public class AccessRequestResourceFromEntityAssembler {
+import com.atlassian.platform.u20231b475.requests.domain.model.aggregates.AccessRequest;
+import com.atlassian.platform.u20231b475.requests.interfaces.rest.resources.AccessRequestResource;
 
+/**
+ * Assembler to convert an Access Request entity to an AccessRequestResource.
+ */
+public class AccessRequestResourceFromEntityAssembler {
+  /**
+   * Converts an Access Request entity to tan AccessRequestResource
+   * 
+   * @param entity The {@link AccessRequest} entity to convert
+   * @return The {@link AccessRequestResource} resource
+   */
+  public static AccessRequestResource toResourceFromEntity(AccessRequest entity) {
+    return new AccessRequestResource(
+        entity.getId(),
+        entity.getEmployeeId(),
+        entity.getRequestedSystem(),
+        entity.getAccessLevel(),
+        entity.getJustification(),
+        entity.getAccessPeriod(),
+        entity.getRequestStatus(),
+        entity.getRequestedAt());
+  }
 }
