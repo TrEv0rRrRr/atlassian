d315b4d75b4dfc7abbd79dc3973f67f220b55da1
Trevor <valentinosolischang@gmail.com>
2025-11-20 13:26:50 -0500

    Add AtlassianApplication class as entry point for Spring Boot application

---
+++ src/main/java/com/atlassian/platform/u20231b475/AtlassianApplication.java
@@ -0,0 +1,13 @@
+package com.atlassian.platform.u20231b475;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlassianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlassianApplication.class, args);
+	}
+
+}
